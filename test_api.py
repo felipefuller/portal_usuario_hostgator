cb2a8b0717691c002a68c9f678f59d0e397d26e7
felipefuller <felipefuller@gmail.com>
2019-11-04 15:18:32 -0300

    add space verification

---
+++ test_api.py
@@ -0,0 +1,11 @@
+import requests
+
+API_URL = 'https://usuarios.taloo.cl/api/1.0'
+API_KEY = 'KTXvLBD7TvoBjVxp9iRyJcJLgWeM3mkS'
+
+headers = {'X-Api-Key': API_KEY}
+
+response = requests.get(
+    '{}/files/Andres_Agurto_-_The_Cookie_Factory5.jpg'.format(API_URL), headers=headers
+)
+print(response.text)
