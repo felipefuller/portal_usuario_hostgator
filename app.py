aa7e37efc9147c889c8199b36ade2460d0f3cfc1
felipefuller <felipefuller@gmail.com>
2019-11-06 00:25:40 -0300

    remove spare upload function

--- app.py
+++ app.py
@@ -359,28 +359,6 @@ def add_article():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload_cv', methods=['GET', 'POST'])
-@is_logged_in
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded')
-            return redirect('/upload_cv')
-        else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-            return redirect(request.url)
-    return render_template('upload.html')
-
 # Edit Article
 @app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
