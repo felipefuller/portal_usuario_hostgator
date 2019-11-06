148fec511550ce71ba8c6c6bdb9de342f7aa5763
felipefuller <felipefuller@gmail.com>
2019-11-06 01:03:06 -0300

    change filename when saving

--- app.py
+++ app.py
@@ -305,7 +305,7 @@ def dashboard():
             cur = mysql.connection.cursor()
 
             # Execute
-            cur.execute("INSERT INTO archivos(nombre_usuario, archivo) VALUES(%s, %s)",(session['username'], filename))
+            cur.execute("INSERT INTO archivos(nombre_usuario, archivo) VALUES(%s, %s)",(session['username'], session['username'] + filename))
 
             # Commit to DB
             mysql.connection.commit()
--- test_api.py
+++ test_api.py
@@ -1,10 +1,15 @@
 import requests
+from datetime import date
 
 API_URL = 'https://usuarios.taloo.cl/api/1.0'
 API_KEY = 'KTXvLBD7TvoBjVxp9iRyJcJLgWeM3mkS'
 
-headers = {'X-Api-Key': API_KEY}
+# headers = {'X-Api-Key': API_KEY}
 
-response = requests.get(
-    '{}/files/Andres_Agurto_-_The_Cookie_Factory5.jpg'.format(API_URL), headers=headers
-)
+# response = requests.get(
+#     '{}/files/Andres_Agurto_-_The_Cookie_Factory5.jpg'.format(API_URL), headers=headers
+# )
+
+today = date.today()
+
+print(str(today.day) + '-' + str(today.month) + '-' + str(today.year))
